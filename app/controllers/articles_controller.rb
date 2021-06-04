088a9ca2a983040c812aef29ff4f01c73b29aa32
Jack Li <luckydog612@163.com>
2021-06-04 09:18:09 +0800

    articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret",
+                               except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -43,7 +46,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 
   private :article_params
